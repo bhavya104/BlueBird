650905a190be3cee793924399c716c6b0c6e0d3f
Magus Pereira <magus.solve@gmail.com>
2020-09-07 19:55:20 -0400

    Update credentials.js

--- public/script/credentials.js
+++ public/script/credentials.js
@@ -1,3 +1,3 @@
 // Crendentials Template: Toggle between remoteDB from false to the address of your server
-var remoteDB = false;
-// var remoteDB = "http://admin:password@127.0.0.1:5984/bluebird";
+// var remoteDB = false;
+var remoteDB = "http://admin:password@127.0.0.1:5984/bluebird";
